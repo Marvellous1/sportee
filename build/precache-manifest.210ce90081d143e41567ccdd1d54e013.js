01268016dc48e3c2b273eef493002ba43813b581
Marvellous <aigbemarvellous01@gmail.com>
2020-06-21 15:23:20 +0100

    changed readme

---
+++ build/precache-manifest.210ce90081d143e41567ccdd1d54e013.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081df71e1f66d554b9b24abcee30327b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba3e08235f8729c16524",
+    "url": "/static/css/main.2e6b97b7.chunk.css"
+  },
+  {
+    "revision": "22a910402b6970f6a81e",
+    "url": "/static/js/2.6b144e2a.chunk.js"
+  },
+  {
+    "revision": "27650def0d6865cedb2014229f52ad5f",
+    "url": "/static/js/2.6b144e2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba3e08235f8729c16524",
+    "url": "/static/js/main.73c2636b.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  },
+  {
+    "revision": "c434c3f5f072d025ddffbe0d002e2efa",
+    "url": "/static/media/basketball.c434c3f5.svg"
+  },
+  {
+    "revision": "dbbd530dddb94b46829385dda8977287",
+    "url": "/static/media/soccer.dbbd530d.svg"
+  }
+]);
